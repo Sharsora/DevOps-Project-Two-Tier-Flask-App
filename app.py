a1c646e7c38aa78dd3a112a2198d8c6f93341f25
Shivangi Harsora <135323873+Sharsora@users.noreply.github.com>
2025-11-28 10:40:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '#DevOpsWithUs\nHello DevOps Enthusiasts, I am Prashant Gohel!'
+    return '#DevOpsWithUs\nHello DevOps Enthusiasts, I am Shivangi!'
